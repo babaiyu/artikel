476208e206b23db5d927d86819bc01b5787cc8e3
babaiyu <bayuputra9a@gmail.com>
2020-08-11 17:09:26 +0700

    Updates

---
+++ _next/static/4TS0-4Dc-aSIGJGm2tyf-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-4116a7d49f2145cc764b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4f0601949c71cd429f69.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
